a605c2d6648200a50b44d73c2e81115c847c9866
ferreusveritas <ferreusveritas@gmail.com>
2019-01-13 14:49:33 -0500

    Fix build system

--- src/main/java/com/ferreusveritas/warpbook/ModConstants.java
+++ src/main/java/com/ferreusveritas/warpbook/ModConstants.java
@@ -2,6 +2,6 @@ package com.ferreusveritas.warpbook;
 
 public class ModConstants{
 	public static final String MODID = "warpbook";
-	public static final String VERSION = "@VERSION@";
+	public static final String VERSION = "1.12.2-9999.9999.9999z";//Maxed out version to satisfy dependencies during dev, Assigned from gradle during build, do not change
 	public static final String NAME = "Warp Book";
 }
